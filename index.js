af330d4ae3ed3ebd494148c1bfe8e4548e8a18b2
destinyF**ker <destinyfunk@163.com>
2024-04-07 12:21:20 +0800

    my-game-of-life initial commit

--- index.js
+++ index.js
@@ -1,3 +1,152 @@
-import * as wasm from "hello-wasm-pack";
+import { Cell, Universe } from "wasm-game-of-life";
+import { memory } from "wasm-game-of-life/wasm_game_of_life_bg.wasm";
 
-wasm.greet();
+const CELL_SIZE = 5;
+const GRID_COLOR = "#cccccc";
+const DEAD_COLOR = "#FFFFFF";
+const ALIVE_COLOR = "#000000";
+const HEIGHT = 128;
+const WIDTH = 256;
+
+let universe = Universe.new(HEIGHT, WIDTH);
+const height = universe.height();
+const width = universe.width();
+
+const playPauseButton = document.getElementById("player-pause");
+const playResetButton = document.getElementById("game-of-life-reset");
+const playEmptyButton = document.getElementById("game-of-life-empty");
+const playSpeedRange = document.getElementById("player-speed");
+
+const canvas = document.getElementById("game-of-life-canvas");
+canvas.height = (CELL_SIZE + 1) * height + 1;
+canvas.width = (CELL_SIZE + 1) * width + 1;
+
+canvas.addEventListener("click", (event) => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+});
+
+const ctx = canvas.getContext("2d");
+
+let animationId = null;
+let fps = 60;
+let timeDifference = 1000 / fps;
+let start;
+const renderLoop = (timestamp) => {
+  // debugger;
+  if (start === undefined) {
+    start = timestamp;
+  }
+  let elapsed = timestamp - start;
+
+  if (elapsed > timeDifference) {
+    universe.tick();
+    start = timestamp;
+  }
+
+  drawGrid();
+  drawCells();
+
+  animationId = requestAnimationFrame(renderLoop);
+};
+
+const isPaused = () => {
+  return animationId === null;
+};
+
+const drawGrid = () => {
+  ctx.beginPath();
+  ctx.strokeStyle = GRID_COLOR;
+
+  // Vertical lines.
+  for (let i = 0; i <= width; i++) {
+    ctx.moveTo(i * (CELL_SIZE + 1) + 1, 0);
+    ctx.lineTo(i * (CELL_SIZE + 1) + 1, (CELL_SIZE + 1) * height + 1);
+  }
+
+  // Horizontal lines.
+  for (let j = 0; j <= height; j++) {
+    ctx.moveTo(0, j * (CELL_SIZE + 1) + 1);
+    ctx.lineTo((CELL_SIZE + 1) * width + 1, j * (CELL_SIZE + 1) + 1);
+  }
+
+  ctx.stroke();
+};
+
+const drawCells = () => {
+  const cellsPtr = universe.cells_ptr();
+  const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
+
+  ctx.beginPath();
+
+  for (let row = 0; row < height; row++) {
+    for (let col = 0; col < width; col++) {
+      const idx = universe.get_index(row, col);
+
+      ctx.fillStyle = cells[idx] === Cell.Dead ? DEAD_COLOR : ALIVE_COLOR;
+      // ctx.fillStyle = bitIsSet(idx, cells) ? ALIVE_COLOR : DEAD_COLOR;
+
+      ctx.fillRect(
+        col * (CELL_SIZE + 1) + 1,
+        row * (CELL_SIZE + 1) + 1,
+        CELL_SIZE,
+        CELL_SIZE
+      );
+    }
+  }
+
+  ctx.stroke();
+};
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", () => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+playResetButton.addEventListener("click", () => {
+  universe = Universe.new(HEIGHT, WIDTH);
+
+  drawGrid();
+  drawCells();
+});
+
+playEmptyButton.addEventListener("click", () => {
+  universe.clear_universe();
+
+  drawGrid();
+  drawCells();
+});
+
+playSpeedRange.addEventListener("input", () => {
+  fps = playSpeedRange.value;
+  timeDifference = 1000 / fps;
+});
+
+play();
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: "./bootstrap.js",
@@ -8,7 +8,16 @@ module.exports = {
     filename: "bootstrap.js",
   },
   mode: "development",
-  plugins: [
-    new CopyWebpackPlugin(['index.html'])
-  ],
+  experiments: {
+    asyncWebAssembly: true, // 启用异步 WebAssembly 实验特性
+  },
+  module: {
+    rules: [
+      {
+        test: /\.wasm$/, // 匹配 WebAssembly 文件
+        type: "webassembly/async", // 设置模块类型为 WebAssembly 异步
+      },
+    ],
+  },
+  plugins: [new CopyWebpackPlugin(["index.html"])],
 };
